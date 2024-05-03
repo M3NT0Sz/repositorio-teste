964ef24eb6afd677ed0febdad204e7ee935ad0ca
M3NT0Sz <matheusmendes2005@outlook.com>
2024-05-03 17:08:11 -0300

    Adicionando Adição, Subtração e Raiz

--- teste.php
+++ teste.php
@@ -1,3 +1,11 @@
 <?php
-    echo  "Olá Mundo!";
-?>
+    $a = 1;
+    $b = 2;
+    $soma = $a + $b;
+    echo $soma;
+
+    $subtracao = $b - $a;
+    echo $subtracao;
+
+    $raiz = sqrt(9);
+    echo $raiz;
