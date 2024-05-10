84da258701f292e152caba1260992907dd67850a
M3NT0Sz <matheusmendes2005@outlook.com>
2024-05-10 15:07:15 -0300

    A

--- teste.php
+++ teste.php
@@ -1,11 +1,2 @@
 <?php
-    $a = 1;
-    $b = 2;
-    $soma = $a + $b;
-    echo $soma;
-
-    $subtracao = $b - $a;
-    echo $subtracao;
-
-    $raiz = sqrt(9);
-    echo $raiz;
+    echo "Abre ai seu corno!";
